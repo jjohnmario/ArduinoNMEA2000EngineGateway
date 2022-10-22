49d72a719ba76a23a09d8fd37404ccaa0fbcaade
John Mario <jjohnmario@gmail.com>
2022-10-21 22:44:17 -0400

    dev

--- ArduinoNMEA2000EngineGateway/ArduinoNMEA2000EngineGateway.ino
+++ ArduinoNMEA2000EngineGateway/ArduinoNMEA2000EngineGateway.ino
@@ -10,8 +10,18 @@
 #include <cstring>
 #include "CAN.h"
 #include "LiquidCrystal_I2C.h"
+#include <avr/pgmspace.h> 
 
 LiquidCrystal_I2C lcd(0x27, 20, 4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+//VDO,12V,EU,0-10 Bar
+double oilPressSensorResistance[] = { 10.00,31.00,52.00,71.00,90.00,107.00,124.00,140.00,156.00,170.00,184.00 };
+double oilPressBar[] = { 0.00,1.00,2.00,3.00,4.00,5.00,6.00,7.00,8.00,9.00,10.00 };
+//VDO,12V,US,100 to 250F
+double engineTempSensorResistance[] = { 9.78,14.15,20.46,29.60,35.48,50.61,71.55,99.00,138.22,199.00,295.68,447.15,676.84,1012.74,1487.36,2137.79,3005.63 };
+double engineTempF[] = { 302.00,284.00,286.00,248.00,230.00,212.00,194.00,176.00,158.00,140.00,122.00,104.00,86.00,68.00,50.00,32.00,14.00 };
+
+bool isFirstScan = true;
+char savedAddr;
 
 // note: the _in array should have increasing values
 double multiMap(double val, double* _in, double* _out, uint8_t size)
@@ -32,30 +42,35 @@ double multiMap(double val, double* _in, double* _out, uint8_t size)
 	return (val - _in[pos - 1]) * (_out[pos] - _out[pos - 1]) / (_in[pos] - _in[pos - 1]) + _out[pos - 1];
 }
 
+void onCanRecieved(int size) {
+
+}
+
 void sendAddressClaim(int addr) {
 
 }
 
-void sendAddressClaimRequest()
+void sendAddressClaimRequest(uint8_t sourceAddr)
 {
+	CAN.beginExtendedPacket(id);
+	CAN.endPacket();
+	delay(1000);//delay while address claims are returned
+
 
 }
 
 int getSavedAddr()
 {
 	//get address from flash memory
+	savedAddr = pgm_read_byte(0x0);
+	return savedAddr;
 }
 
-
-//VDO,12V,EU,0-10 Bar
-double oilPressSensorResistance[] = { 10.00,31.00,52.00,71.00,90.00,107.00,124.00,140.00,156.00,170.00,184.00 };
-double oilPressBar[] = { 0.00,1.00,2.00,3.00,4.00,5.00,6.00,7.00,8.00,9.00,10.00 };
-//VDO,12V,US,100 to 250F
-double engineTempSensorResistance[] = { 9.78,14.15,20.46,29.60,35.48,50.61,71.55,99.00,138.22,199.00,295.68,447.15,676.84,1012.74,1487.36,2137.79,3005.63 };
-double engineTempF[] = {302.00,284.00,286.00,248.00,230.00,212.00,194.00,176.00,158.00,140.00,122.00,104.00,86.00,68.00,50.00,32.00,14.00 };
-
-bool firstScan = true;
-int savedAddr;
+void firstScan() {
+	sendAddressClaimRequest();
+	savedAddr = getSavedAddr();
+	if(savedAddr == 0)
+}
 
 // the setup function runs once when you press reset or power the board
 void setup() 
@@ -82,7 +97,8 @@ void setup()
 		lcd.setCursor(1, 0);
 		lcd.print("CAN BUS ONLINE!");
 	}
-
+	CAN.onReceive(onCanRecieved);
+	lcd.clear();
 
 }
 
@@ -90,13 +106,16 @@ void setup()
 void loop()
 {
 	//First pass
-	if (firstScan) {
-		sendAddressClaimRequest();
+	if (isFirstScan) {
+		firstScan();
+		//sendAddressClaimRequest();
 		//CAN.available
 		//sendAddressClaim(getSavedAddr());
 
 		firstScan = false;
 	}
+
+
 	// send packet: id is 11 bits, packet can contain up to 8 bytes of data
 	Serial.print("Sending packet ... ");
 
@@ -109,8 +128,6 @@ void loop()
 	CAN.endPacket();
 
 	Serial.println("done");
-
-	delay(1000);
 
 	// send extended packet: id is 29 bits, packet can contain up to 8 bytes of data
 	Serial.print("Sending extended packet ... ");
@@ -125,26 +142,32 @@ void loop()
 
 	Serial.println("done");
 
-	delay(1000);
-	//double vIn = 3.30;
-	//double r1 = 1000.00;
-	//double r2 = 0.00;
-	//double val = 0.00;
 
-	////Coolant temp
-	//Serial.println(analogRead(A0));
-	//r2 = voltageToResistance(analogRead(A0), vIn, r1);
-	//val = multiMap(r2, engineTempSensorResistance, engineTempF,11);
+	double vIn = 3.30;
+	double r1 = 1000.00;
+	double r2 = 0.00;
+	double val = 0.00;
+
+	//Coolant temp
+	Serial.println(analogRead(A0));
+	r2 = voltageToResistance(analogRead(A0), vIn, r1);
+	val = multiMap(r2, engineTempSensorResistance, engineTempF,11);
 	//Serial.print("The engine temp is:");
 	//Serial.print(val);
 	//Serial.println("F");
+	lcd.setCursor(1, 0);
+	lcd.print("WATER F:");
+	lcd.print(val);
 
-	////Oil pressure
-	//r2 = voltageToResistance(analogRead(A1), vIn, r1);
-	//val = multiMap(r2, oilPressSensorResistance, oilPressBar, 11);
+	//Oil pressure
+	r2 = voltageToResistance(analogRead(A1), vIn, r1);
+	val = multiMap(r2, oilPressSensorResistance, oilPressBar, 11);
 	//Serial.print("The engine oil pressure is:");
 	//Serial.print(val);
 	//Serial.println("Bar");
+	lcd.setCursor(1, 1);
+	lcd.print("OIL BAR:");
+	lcd.print(val);
 }
 
 double voltageToResistance(int countsIn, double vIn, double r1)
