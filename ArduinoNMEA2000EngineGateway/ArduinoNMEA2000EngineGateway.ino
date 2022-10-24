40732ac22017a9561386e23997b20c811f66ca7c
John Mario <jjohnmario@gmail.com>
2022-10-23 22:21:34 -0400

    ongiong dev

--- ArduinoNMEA2000EngineGateway/ArduinoNMEA2000EngineGateway.ino
+++ ArduinoNMEA2000EngineGateway/ArduinoNMEA2000EngineGateway.ino
@@ -19,13 +19,19 @@ double oilPressBar[] = { 0.00,1.00,2.00,3.00,4.00,5.00,6.00,7.00,8.00,9.00,10.00
 //VDO,12V,US,100 to 250F
 double engineTempSensorResistance[] = { 9.78,14.15,20.46,29.60,35.48,50.61,71.55,99.00,138.22,199.00,295.68,447.15,676.84,1012.74,1487.36,2137.79,3005.63 };
 double engineTempF[] = { 302.00,284.00,286.00,248.00,230.00,212.00,194.00,176.00,158.00,140.00,122.00,104.00,86.00,68.00,50.00,32.00,14.00 };
-
+byte NAME[4] = { 0x0,0x0,0x0,0x0 };
 bool isFirstScan = true;
 char savedAddr;
 union {
 	long val;
 	uint8_t bytes[4];
 } extendedPacketId;
+typedef struct {
+	byte priority;//0=Highest,7=Lowest
+	bool msgType;//0=ISO,1=NMEA2000
+	unsigned pgn;
+	byte sourceAddr;//Adress of producing CA
+}Extended_Id;
 
 // note: the _in array should have increasing values
 double multiMap(double val, double* _in, double* _out, uint8_t size)
@@ -49,36 +55,67 @@ double multiMap(double val, double* _in, double* _out, uint8_t size)
 void onCanRecieved(int size) {
 	//Extended packet
 	if (CAN.packetExtended()) {
-		Serial.print("Extended packet recieved from address:");
 		extendedPacketId.val = CAN.packetId();
 		if (sizeof(extendedPacketId.bytes) == 4) {
-			Serial.println(extendedPacketId.bytes[3]);
-			parseCanExtendedId(extendedPacketId.bytes);
+			Serial.print("Extended packet recieved from address:");
+			Serial.println(extendedPacketId.bytes[0]);
+			Extended_Id extId = parseCanExtendedId(extendedPacketId.bytes);
+			int dataLen = CAN.available();
+			uint8_t data[8];
+			CAN.readBytes(data, dataLen);
+			for (byte b : data)
+				Serial.print((char)b);
+			if (extId.msgType == 0)
+				processIsoMsg(extId,data);//Message is ISO
+			else
+				processNmea2000Msg(extId,data);//Message is NMEA2000
 		}
 	}
-
-
-	while (CAN.available()) {
-		Serial.print((char)CAN.read());
-	}
 }
 
-void parseCanExtendedId(byte* packetIdBytes)
+void processIsoMsg(Extended_Id extId,byte* data) {
+	Serial.println("Message is ISO-formatted");
+	//Allowed pgns
+}
+
+void processNmea2000Msg(Extended_Id extId, byte* data) {
+	Serial.println("Message is NMEA2000-formatted");
+
+}
+
+
+Extended_Id parseCanExtendedId(byte* idFrame)
 {
+	//idFrame[0] = byte[3] of CAN ID frame
+	//idFrame[1] = byte[2] of CAN ID frame
+	//idFrame[2] = byte[1] of CAN ID frame
+	//idFrame[3] = byte[0] of CAN ID frame
 	Serial.print("Pgn:");
-
-	//If byte 1 is 
-
-	if (sizeof(packetIdBytes) == 4) {
-		unsigned pgn = (pgn << 8) + packetIdBytes[2];
-		pgn = (pgn << 8) + packetIdBytes[1];
-		pgn = (pgn << 8) + packetIdBytes[0];
-		Serial.println(pgn);
+	Extended_Id extId;
+	if (sizeof(idFrame) == 4) {
+		Extended_Id extId;
+		extId.msgType = (0x1 & idFrame[3]);//msg type
+		extId.pgn = extId.msgType;
+		extId.pgn = (extId.pgn << 8) + idFrame[2];//pf
+		byte ps;
+		if (idFrame[1] > 239)
+			ps = 0x0;
+		else
+			ps = idFrame[1];
+		extId.pgn = (extId.pgn << 8) + ps;//ps
+		Serial.println(extId.pgn);
+		extId.sourceAddr = idFrame[0];//source address
 	}
+	return extId;
 }
 
-void sendAddressClaim(int addr) {
-
+byte* makeAddressClaim(int addr) {
+	//For now set NAME to zero
+	byte bytes[4];
+	bytes[0] = 0x0;
+	bytes[1] = 0x0;
+	bytes[2] = 0x0;
+	bytes[3] = 0x0;
 }
 
 void sendAddressClaimRequest(uint8_t sourceAddr)
@@ -164,7 +201,7 @@ void loop()
 	// send extended packet: id is 29 bits, packet can contain up to 8 bytes of data
 	Serial.print("Sending extended packet ... ");
 
-	CAN.beginExtendedPacket(0x1F503);
+	CAN.beginExtendedPacket(0x18EE0005);
 	CAN.write('w');
 	CAN.write('o');
 	CAN.write('r');
