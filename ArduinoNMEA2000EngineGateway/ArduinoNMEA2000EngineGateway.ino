b5e2b3591cbc71102db6a76e342738a2627e7bcd
John Mario <jjohnmario@gmail.com>
2022-12-04 16:12:48 -0500

    Added interrupt for 500ms PGN Tx

--- ArduinoNMEA2000EngineGateway/ArduinoNMEA2000EngineGateway.ino
+++ ArduinoNMEA2000EngineGateway/ArduinoNMEA2000EngineGateway.ino
@@ -13,7 +13,11 @@
 #include "CAN.h"
 #include "LiquidCrystal_I2C.h"
 #include <avr/pgmspace.h> 
+#include <avr/interrupt.h>
 
+uint8_t pgnTxDelayCtr = 0x0;
+uint16_t addrClaimCtr = 0x0;
+bool addrConflict = false;
 uint8_t pgn126996FastPacketSeq = 0x0;
 uint8_t pgn126464FastPacketSeq = 0x64;
 unsigned long lastMillis;
@@ -118,6 +122,7 @@ void onCanRecieved(int size) {
 	}
 }
 
+//Create action to perform
 void processN2kMsg(Extended_Id extId, byte* data)
 {
 	//ISO PGN Request 59904
@@ -227,9 +232,11 @@ void negotiateAddressClaim(byte srcAddr, byte destAddr, uint64_t name) {
 		*/
 		//Compare NAME of this device and the calling device.
 		if (srcAddr == claimedAddr) {
+			addrClaimCtr = 0;
 			if (thisCanNAME > name) {
-				Serial.println("Address conflict detected. Claiming new address...");
+				Serial.print("Address conflict detected. Claiming new address: ");
 				claimedAddr++;
+				Serial.println(claimedAddr);
 				sendAddressClaim(claimedAddr, 255, thisCanNAME);
 			}
 		}
@@ -643,8 +650,6 @@ void writePgn130576(byte destAddr){
 	CAN.endPacket();
 }
 
-
-
 //Creates CAN device NAME
 uint64_t createDeviceName(uint32_t mfrCode,uint32_t serial,uint8_t funcInst,
 	uint8_t ecuInst,uint8_t function, uint8_t vehicleSys,uint8_t indGroup, uint8_t vehicleSysInst)
@@ -722,6 +727,58 @@ void updateLcd(double waterTemp, double oilPress) {
 	lcd.print(waterTemp, 0);
 }
 
+// Set timer TC4 to call the TC4_Handler every 5ms: (8 * (29999 + 1)) / 48MHz = 5ms
+void setupTc4Timer() {
+	PORT->Group[PORTA].DIRSET.reg = PORT_PA21;// Set D7 as a digital output
+
+	GCLK->CLKCTRL.reg = GCLK_CLKCTRL_CLKEN | // Enable GCLK0 for TC4 and TC5
+		GCLK_CLKCTRL_GEN_GCLK0 |             // Select GCLK0 at 48MHz
+		GCLK_CLKCTRL_ID_TC4_TC5;             // Feed GCLK0 output to TC4 and TC5
+	while (GCLK->STATUS.bit.SYNCBUSY);       // Wait for synchronization
+
+	TC4->COUNT16.CC[0].reg = 29999;          // Set the TC4 CC0 register as the TOP value in match frequency mode
+	while (TC4->COUNT16.STATUS.bit.SYNCBUSY);// Wait for synchronization
+
+	NVIC_SetPriority(TC4_IRQn, 0);    // Set the Nested Vector Interrupt Controller (NVIC) priority for TC4 to 0 (highest)
+	NVIC_EnableIRQ(TC4_IRQn);         // Connect TC4 to Nested Vector Interrupt Controller (NVIC)
+
+	TC4->COUNT16.INTENSET.reg = TC_INTENSET_OVF;// Enable TC4 overflow (OVF) interrupts
+
+	TC4->COUNT16.CTRLA.reg |= TC_CTRLA_PRESCSYNC_PRESC |     // Reset timer on the next prescaler clock
+		TC_CTRLA_PRESCALER_DIV8 |      // Set prescaler to 8, 48MHz/8 = 6MHz
+		TC_CTRLA_WAVEGEN_MFRQ |        // Put the timer TC4 into match frequency (MFRQ) mode 
+		TC_CTRLA_MODE_COUNT16;         // Set the timer to 16-bit mode      
+	while (TC4->COUNT16.STATUS.bit.SYNCBUSY);                // Wait for synchronization
+
+	TC4->COUNT16.CTRLA.bit.ENABLE = 1;                       // Enable the TC4 timer
+	while (TC4->COUNT16.STATUS.bit.SYNCBUSY);                // Wait for synchronization
+}
+
+//PGN Tx and Address Claim Timer Handler
+void TC4_Handler() {                                         // Interrupt Service Routine (ISR) for timer TC4
+	if (TC4->COUNT16.INTFLAG.bit.OVF && TC4->COUNT16.INTENSET.bit.OVF)       // Optionally check for overflow (OVF) interrupt      
+	{
+		PORT->Group[PORTA].OUTTGL.reg = PORT_PA21;             // Toggle the D7 output
+		TC4->COUNT16.INTFLAG.reg = TC_INTFLAG_OVF;             // Clear the OVF interrupt flag
+
+		//PGN Tx delay interval 500 ms (5ms x 100)
+		if (pgnTxDelayCtr < 100)
+			pgnTxDelayCtr++;
+		else {
+			//5000ms since last address claim conflict which implies the conflict has been resolved.
+			if (addrClaimCtr >= 1000) {
+				{
+					writePgn127245(255);
+					writePgn130576(255);
+					pgnTxDelayCtr = 0;
+				}
+			}
+		}	
+		if (addrClaimCtr < 1000)
+			addrClaimCtr++;
+	}
+}
+
 // the setup function runs once when you press reset or power the board
 void setup(){
 	// initialize the lcd 
@@ -733,6 +790,9 @@ void setup(){
 	lcd.setCursor(1, 1);
 	lcd.print("BOOTING...");
 	//delay(3000);
+
+	//Setup timer interrupt of 500 milliseconds
+	setupTc4Timer();
 
 	//Create CAN device NAME
 	thisCanNAME = createDeviceName(2046, 1, 0, 0, 130, 25, 4, 0);
@@ -760,12 +820,11 @@ void setup(){
 	//Send address claim
 	sendAddressClaim(claimedAddr, 255, thisCanNAME);
 }
+
 // the loop function runs over and over again until power down or reset
 void loop()
 {
-	writePgn127245(255);
-	writePgn130576(255);
-	delay(500);
+	//delay(500);
 	//double vInMin = 0.0;
 	//double vInMax = 3.3;
 	//double vIn = 5.0;
